8e13fd305d42bd540b1365ce4684c2a61f0976f6
Ayman Abdelazez <aymanaea52@gmail.com>
2021-01-31 23:09:50 +0200

    lineGraph component

--- src/Components/Sidebar/Table/Table.js
+++ src/Components/Sidebar/Table/Table.js
@@ -1,27 +1,31 @@
 import React from 'react';
 
 import classes from './Table.module.css';
+import Aux from "../../../hoc/Auxiliry/Auxiliry";
 
 const Table = ({ tableInfo }) => {
     return (
-        <div className={classes.Table}>
-            <table>
-                <tbody>
-                    {tableInfo.map((element, index) => (
-                        <tr key={index}>
-                            <td>
-                                {element.country}
-                            </td>
-                            <td>
-                                <strong>
-                                    {element.cases}
-                                </strong>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <Aux>
+            <h3>Live cases by country</h3>
+            <div className={classes.Table}>
+                <table>
+                    <tbody>
+                        {tableInfo.map((element, index) => (
+                            <tr key={index}>
+                                <td>
+                                    {element.country}
+                                </td>
+                                <td>
+                                    <strong>
+                                        {element.cases}
+                                    </strong>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </Aux>
     )
 }
 
